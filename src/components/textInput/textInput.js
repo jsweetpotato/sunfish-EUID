f2aae2f11c2051ec2c9b302f7a28b8c2c3cac592
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-01-09 16:43:10 +0900

    거지같은 대소문자 오류 해결

--- src/components/textInput/textInput.js
+++
@@ -1,9 +0,0 @@
-const textValue = document.querySelector('#contentName');
-const span = document.querySelector('#letterCount');
-
-function getValue() {
-  const { value } = textValue;
-  span.textContent = `${value.length}/24`;
-}
-
-textValue.addEventListener('input', getValue);
