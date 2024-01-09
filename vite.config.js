83981c72c405c7dea37f3d96c09c1809444ff74b
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-01-09 11:04:59 +0900

    [fix: 페이지 연결 오류 수정]
    각 페이지 vite.config.js에 dirName으로 등록

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,19 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        // 마이페이지
         myeuid: resolve(__dirname, 'src/pages/MyEuid/index.html'),
+        myProfile: resolve(__dirname, 'src/pages/MyEuid/MyProfile.html'),
+        editProfile: resolve(__dirname, 'src/pages/MyEuid/EditProfile.html'),
+        // 로그인
         login: resolve(__dirname, 'src/pages/login/index.html'),
+        category: resolve(__dirname, 'src/pages/login/category.html'),
+        ouath: resolve(__dirname, 'src/pages/login/ouath.html'),
+        // 게시판
         board: resolve(__dirname, 'src/pages/board/index.html'),
+        together: resolve(__dirname, 'src/pages/board/together.html'),
+        writeTogether: resolve(__dirname, 'src/pages/board/writeTogether.html'),
+        // 기기거래
         exchange: resolve(__dirname, 'src/pages/exchange/index.html'),
       },
     },
