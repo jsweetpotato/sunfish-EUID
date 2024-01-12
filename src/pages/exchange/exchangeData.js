2c0a88a7667b73ee5aaef5651b7c679478dcd093
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-01-12 14:50:11 +0900

    [refactor: 코드 리팩터링]

--- src/pages/exchange/exchangeData.js
+++ src/pages/exchange/exchangeData.js
@@ -4,42 +4,41 @@ import { getNode, getPbImageURL, comma } from '/src/lib';
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 const section = getNode('.content');
 
-function handleHeartClick(event) {
-  const {target} = event;
+function handleHeartClick({ target }) {
+  const heartBG = target.querySelector('.heart-bg');
 
   if (target.classList.contains('heartContainer')) {
-    const countElement = target.nextElementSibling; 
+    const countElement = heartBG.nextElementSibling;
     let count = Number(countElement.innerText) || 0;
 
-    if (target.classList.contains('bg-heart-icon')) {
-      count+=1;
-      target.classList.add('bg-heart-full-icon');
-      target.classList.remove('bg-heart-icon');
+    if (heartBG.classList.contains('bg-heart-icon')) {
+      count += 1;
+      heartBG.classList.add('bg-heart-full-icon');
+      heartBG.classList.remove('bg-heart-icon');
     } else {
-      count = Math.max(count - 1, 0); 
-      target.classList.remove('bg-heart-full-icon');
-      target.classList.add('bg-heart-icon');
+      count = Math.max(count - 1, 0);
+      heartBG.classList.remove('bg-heart-full-icon');
+      heartBG.classList.add('bg-heart-icon');
     }
 
     countElement.innerText = count;
   }
 }
 
-
 export default async function imageList() {
   const result = await pb
     .collection('selling')
     .getFullList('pb/collections/selling');
 
-
   result.forEach((value) => {
     const sharing =
       value.tradingType === 'nanum'
-        ? `<span class="text-label-sm text-white rounded-[4px] gap-2 grow bg-no-repeat py-1 px-2 bg-secondary">나눔</span>`: 
-        '';
-        section.insertAdjacentHTML(
-          'afterbegin' /* html */,
-          `
+        ? `<span class="text-label-sm text-white rounded-[4px] gap-2 grow bg-no-repeat py-[2px] px-1 bg-secondary">나눔</span>`
+        : '';
+    section.insertAdjacentHTML(
+      'afterbegin',
+      /* html */
+      `
       <div class="flex p-3 gap-3 justify-center items-center border-b cursor-pointer relative" id="${
         value.id
       }">
@@ -49,12 +48,11 @@ export default async function imageList() {
             'productImages'
           )}" alt="${value}" class="w-[95px] h-[95px] object-cover grow rounded-lg">
         </figure>
-        <div class="flex flex-col grow p-3">
-          <a href="/src/pages/exchange/exchangeDetail.html?id=#${
-            value.id
-          }" class="text-paragraph-md absolute w-full h-full top-0 left-0"><span class='absolute top-5 left-32'>${
-            value.title
-          }</span></a>
+        <div class="flex flex-col grow p-3 ">
+          <a href="/src/pages/exchange/exchangeDetail.html?id=#${value.id}" 
+          class="text-paragraph-md absolute w-full h-full top-0 left-0">
+          <span class='absolute top-5 left-32'>${value.title}</span>
+          </a>
           <span class="text-paragraph-sm" aria-label="판매위치 • 작성시간">마포구 신수동 • 44분전</span>
           <div class="gap-1 grow w-full items-center">
             ${sharing}
@@ -62,16 +60,15 @@ export default async function imageList() {
               value.price
             )}원</span>
           </div>
-          <div class="flex gap-[1px] absolute right-1 bottom-0 pb-1 items-center">
-            <button type="button" class="heartContainer bg-heart-icon p-3 bg-no-repeat" aria-label="좋아요 표시하기"></button>
-            <span class="greatCount text-paragraph-sm heartCount">0</span>
-          </div>
+          <button type="button" class="heartContainer flex h-8 gap-[1px] min-w-8 absolute right-3 bottom-2 pb-1 items-center justify-end">
+            <span class="heart-bg bg-heart-icon p-2 bg-contain bg-no-repeat" aria-label="좋아요 표시하기"></span>
+            <span class="text greatCount text-paragraph-sm text-right font-sans heartCount">0</span>
+          </button>
         </div>
       </div>
       `
-        );
-  }); 
-};
-
+    );
+  });
+}
 
 section.addEventListener('click', handleHeartClick);
--- src/pages/exchange/exchangeDetail.js
+++ src/pages/exchange/exchangeDetail.js
@@ -12,40 +12,54 @@ const main = getNode('#main');
 const productInfo = getNode('#productInfo');
 const footer = getNode('#footer');
 const watchTogether = getNode('#watchTogether');
-const back = getNode('#back')
+const back = getNode('#back');
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
-export default async function getData(){
+export default async function getData() {
   const hash = window.location.hash.slice(1);
-  
-  const avatarList = await pb.collection('selling').getOne(hash, {expand: 'profile.userId'});
-    
+
+  const avatarList = await pb
+    .collection('selling')
+    .getOne(hash, { expand: 'profile.userId' });
+
   const { title, description, price } = avatarList;
   const users = avatarList.expand.profile.expand.userId;
   const { name } = users;
 
   main.insertAdjacentHTML(
-      'afterbegin', /* html */
-      `
+    'afterbegin' /* html */,
+    `
   <div class="swiper w-full flex grow flex-shrink" >
     <div class="swiper-wrapper">
-      <div class="swiper-slide "><img src="${getPbImageURL(avatarList,'productImages')}" alt="상품 이미지" class='w-full h-[305px]'></div>
-      <div class="swiper-slide "><img src="${getPbImageURL(avatarList,'productImages')}" alt="상품 이미지" class='w-full h-[305px]'></div>
-      <div class="swiper-slide "><img src="${getPbImageURL(avatarList,'productImages')}" alt="상품 이미지" class='w-full h-[305px]'></div>
+      <div class="swiper-slide "><img src="${getPbImageURL(
+        avatarList,
+        'productImages'
+      )}" alt="상품 이미지" class='w-full h-[305px] object-cover'></div>
+      <div class="swiper-slide "><img src="${getPbImageURL(
+        avatarList,
+        'productImages'
+      )}" alt="상품 이미지" class='w-full h-[305px] object-cover'></div>
+      <div class="swiper-slide "><img src="${getPbImageURL(
+        avatarList,
+        'productImages'
+      )}" alt="상품 이미지" class='w-full h-[305px] object-cover'></div>
     </div>
     <div class="swiper-pagination"></div>
   
   </div>
 `
-    );
+  );
 
   profileInfo.insertAdjacentHTML(
-    'afterbegin' ,/* html */
+    'afterbegin' /* html */,
     `
       <div class="flex justify-center items-center gap-2">
         <figure>
-          <img src="${getPbImageURL(users, 'avatar')}" alt="" class="w-10 h-10 border rounded-full bg-contents-content-secondary">
+          <img src="${getPbImageURL(
+            users,
+            'avatar'
+          )}" alt="" class="w-10 h-10 border rounded-full bg-contents-content-secondary">
         </figure>
         <div class="flex flex-col justify-center items-start">
           <span class="text-label-md" aria-label="프로필 이름">${name}</span>
@@ -56,7 +70,7 @@ export default async function getData(){
   );
 
   productInfo.insertAdjacentHTML(
-    'afterbegin', /* html */
+    'afterbegin' /* html */,
     `
       <div class="flex flex-col items-start gap-3">
         <h1 class="text-label-lg">${title}</h1>
@@ -68,7 +82,7 @@ export default async function getData(){
   );
 
   footer.insertAdjacentHTML(
-    'afterbegin' , /* html */
+    'afterbegin' /* html */,
     `
   <button
     type="button"
@@ -98,43 +112,46 @@ export default async function getData(){
   });
 }
 
-function changeHeart(e){
-  const { target }  = e;
-  if(target.classList.contains('heartContainer')){
-    if(target.classList.contains('heart')) {
-        target.classList.toggle('bg-heart-full-icon');
-        target.classList.toggle('bg-heart-icon');
-      }
-  } 
+function changeHeart(e) {
+  const { target } = e;
+  if (target.classList.contains('heartContainer')) {
+    if (target.classList.contains('heart')) {
+      target.classList.toggle('bg-heart-full-icon');
+      target.classList.toggle('bg-heart-icon');
+    }
+  }
 }
 
-async function watch(){
+async function watch() {
   const watchList = await pb.collection('selling').getList(1, 6);
-  console.log(watchList)
-  
+  console.log(watchList);
+
   watchList.items.forEach((item) => {
     watchTogether.insertAdjacentHTML(
-      'afterbegin' /* html */,
-      `
-      <div class=" pb-[10px] flex-col gap-[14px] w-full">
-        <figure>
-          <img src="${getPbImageURL(
-            item,
-            'productImages'
-          )}" alt="상품 이미지" class="flex w-40 h-40 object-cover grow border rounded-t-lg">
+      'afterbegin',
+      /* html */ `
+      <article class="relative aspect-[1/1.38] rounded-lg shadow-[4px_4px_16px_0px_rgba(0,0,0,0.08),0px_1px_4px_0px_rgba(0,0,0,0.15)] hover:shadow-gray-300 transition-all duration-200">
+        <figure class="h-1/2">
+          <img class="w-full h-full object-cover rounded-t-lg bg-contents-content-secondary"
+          src="${getPbImageURL(item, 'productImages')}" alt="상품 이미지" ">
         </figure>
-        <div class="flex flex-col items-center self-stretch">
-          <h3 class="text-label-sm self-stretch">${item.title}</h3>
-          <h3 class="text-label-sm self-stretch">${comma(item.price)}원</h3>
-        </div>
-      </div>
+        <a class="absolute top-0 left-0 w-full h-full" href="">
+          <span class="absolute w-full top-[55%] px-2">
+            <span class="text-ellipsis line-clamp-2 text-paragraph-sm">
+              ${item.title}
+            </span>
+            <span
+              class="mb-1 text-label-sm aria-label="판매가격"
+              >${comma(item.price)}원</span>
+          </span>
+      </a>
+      </article>
     `
     );
-  })
+  });
 }
 
-
-watch()
+watch();
 getData();
 footer.addEventListener('click', changeHeart);
 back.addEventListener('click', () => history.back());
