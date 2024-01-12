fa6e0c010d2b9b1b96ffe777936c675042068059
jykim29 <skt13181@naver.com>
2024-01-13 00:54:31 +0900

    feat : 게시판 메인 pocketbase 데이터 연동
    - 관심분야 모달 마크업 수정
    - 임시 더미데이터 삭제
    - pocketbase 연동 데이터 렌더링 로직 구현
    - 관심분야에 따른 pocketbase 필터링된 데이터 렌더링 로직 구현

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -1,193 +1,279 @@
-import gsap from 'gsap';
+/* eslint-disable no-alert, no-shadow, import/no-unresolved, import/extensions, import/no-absolute-path */
 
-/**
- * @type {{ together: {id:number, title: string, age: string, datetime:string, local: string, createdAt: number, maxMember: number, currentMember: string[] }; qna: {}; }}
- */
-const dummyData = {
-  together: [
-    {
-      id: 0,
-      title: 'youtube 클론 프젝 같이하실분~',
-      age: '누구나 참여가능',
-      datetime: '내일, 오후 7:00',
-      local: '응암동',
-      createdAt: new Date().getTime(),
-      maxMember: 3,
-      currentMember: ['kim', 'lee'],
-    },
-    {
-      id: 1,
-      title: '멋사플레이스 클론스터디 하실분',
-      age: '20대',
-      datetime: '오늘, 오후 8:00',
-      local: '비전동',
-      createdAt: new Date().getTime(),
-      maxMember: 4,
-      currentMember: ['jo'],
-    },
-    {
-      id: 2,
-      title: '알고리즘 매일 아침마다 인증할 분',
-      age: '누구나 참여 가능',
-      datetime: '5월 1일, 오전 10:00',
-      local: '통복동',
-      createdAt: new Date().getTime(),
-      maxMember: 5,
-      currentMember: ['jo', 'kang', 'choi'],
-    },
-  ],
-  qna: [
-    {
-      id: 0,
-      title:
-        '코딩 입문한지 얼마안된 초보입니다. (자바스크립트 클로저에 대한 질문)',
-      description:
-        '안녕하세요. 디자인 전공으로 종사하다가 코딩쪽으로 스펙업을 하고 싶어서 입문하게된 코린이입니다. 독학으로 자바스크립트 공부중인데요. chtatGPT라는 정말 좋은 선생님이 생겨서 공부하기 정말 좋은 시대라고 생각하고 공부 하고 있습니다. 입문한지는 며칠안되어서 너무 기본적인 것일 수도 있겠지만',
-      local: '연희동',
-      createdAt: new Date().getTime(),
-      views: 12,
-      imgUrl: [
-        '/src/assets/boardIcon/sampleImg.png',
-        '/src/assets/boardIcon/sampleImg.png',
-      ],
-    },
-    {
-      id: 1,
-      title: '자바스크립트 마우스 이벤트 질문',
-      description:
-        '자바스크립트로 아날로그 타이머를 만들고 있었는데 이해가 잘안되는 부분이 생겼습니다.',
-      local: '연희동',
-      createdAt: new Date().getTime(),
-      views: 181,
-      imgUrl: [
-        '/src/assets/boardIcon/sampleImg.png',
-        '/src/assets/boardIcon/sampleImg.png',
-      ],
-    },
-    {
-      id: 2,
-      title: '유효성 인식 문제 (자바스크립트 )',
-      description:
-        '안녕하세요 우편번호 찾기 버튼 없이 숫자를 직접 넣었을 때는 아래 이미지와 같이 잘 됩니다. 그런데 [우편번호 찾기] 버튼으로 우편 번호 검색을 한 후 우편번호 필드에 출력은 되는데 바로 유효성 인식을 하지 못하는 상황 입니다.',
-      local: '중앙동',
-      createdAt: new Date().getTime(),
-      views: 11,
-      imgUrl: [
-        '/src/assets/boardIcon/sampleImg.png',
-        '/src/assets/boardIcon/sampleImg.png',
-      ],
-    },
-  ],
+import gsap from 'gsap';
+import { pb, getNode, getNodes, insertLast } from '/src/lib/';
+
+/*
+  init함수 : 로컬스토리지 관심분야 불러오고 객체에 할당
+*/
+
+const interestsState = {
+  programming: false,
+  design: false,
+  uiux: false,
+  frontend: false,
+  backend: false,
+  publishing: false,
+  ai: false,
+  blockchain: false,
 };
 
-function createTemplate(data) {
-  const { together, qna } = data;
-  const togetherTemplateArray = [];
-  const qnaTemplateArray = [];
-  together.forEach((item) => {
-    const { title, age, datetime, local, createdAt, maxMember, currentMember } =
-      item;
-    const template = /* html */ `
+function createTogetherTemplate(item) {
+  const { age, category, date, gender, id, members, title, owner, created } =
+    item;
+  let { maxMember } = item;
+  maxMember = maxMember === '제한없음' ? maxMember : `${maxMember}명`;
+  const template = /* html */ `
     <li  class="hover:bg-gray-100 transition-all">
     <div
-      class="relative p-3 flex flex-col justify-center items-start gap-1 border-b border-contents-content-secondary"
-      
-    >
+      class="relative p-3 flex flex-col justify-center items-start gap-1 border-b border-contents-content-secondary">
+    <div class="flex items-center gap-1 mb-7">
       <span
-        class="text-label-sm px-1 mb-7 bg-bluegray-600 text-white rounded"
-        >같이해요</span
-      >
-      <a href="/src/pages/board/togetherView.html"
-        class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary "
-      >
+        class="text-label-sm px-1 bg-bluegray-600 text-white rounded"
+        >같이해요</span>
+      <span
+        class="text-label-sm px-1 bg-tertiary text-white rounded"
+        >${category}</span>
+    </div>
+      <a href="/src/pages/board/togetherView.html?id=${id}"
+        class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary ">
         <span class="absolute top-8 left-3 w-[90%] overflow-hidden whitespace-nowrap text-ellipsis">${title}</span>
       </a>
       <span
         class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-people_full-icon bg-no-repeat bg-left"
-        >${age}</span
-      >
+        >${age}, ${gender}</span>
       <span
         class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-calender-icon bg-no-repeat bg-left"
-        >${datetime}</span
-      >
+        >${new Date(date).toLocaleDateString()}</span>
       <div class="w-full flex justify-between">
         <span class="text-paragraph-sm font-normal text-gray-600"
-          >${local} · 9분 전</span
-        >
+          >연희동 · ${new Date(created).toLocaleDateString()}</span>
         <span
           class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-people-icon bg-no-repeat bg-left"
-          >${currentMember.length}/${maxMember}명</span
-        >
+          >${members.memberId.length}/${maxMember}</span>
       </div>
     </div>
     </li>
     `;
-    togetherTemplateArray.push(template);
-  });
-  qna.forEach((item) => {
-    const { title, description, local, createdAt, views, imgUrl } = item;
-    const template = /* html */ `
-<li class="hover:bg-gray-100 transition-all">
-<div
-  class="relative p-3 border-b flex flex-row justify-between gap-1 border-contents-content-secondary"
-  
->
-  <div
-    class="w-[calc(100%-70px)] flex flex-col flex-shrink-1 justify-center items-start gap-1"
-  >
-    <div class="flex items-center gap-1 mb-7">
-      <span
-        class="text-label-sm px-1 bg-bluegray-600 text-white rounded"
-        >질의응답</span
-      >
-      <span
-        class="text-label-sm px-1 bg-tertiary text-white rounded"
-        >인기</span
-      >
-    </div>
-    <a href=""
-      class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary "
-    >
-      <span class="absolute top-8 left-3 w-[70%] overflow-hidden whitespace-nowrap text-ellipsis">${title}</span>
-    </a>
-    <span
-      class="w-full text-paragraph-sm font-normal text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis"
-      >${description.slice(0, 50)}...</span
-    >
-    <span class="text-paragraph-sm font-normal text-gray-600"
-      >${local} · 9분 전 · 조회 ${views}</span
-    >
-  </div>
-  <div
-    class="w-[70px] min-w-[70px] flex justify-center items-center"
-  >
+
+  return template;
+}
+function createQnaTemplate(item) {
+  const { category, title, description, imgField, comments, views, created } =
+    item;
+  const imgUrl =
+    imgField.length === 0
+      ? ''
+      : pb.files.getUrl(item, imgField[0], { thumb: '0x60' });
+
+  const template = /* html */ `
+    <li class="hover:bg-gray-100 transition-all">
     <div
-      class="w-[60px] h-[60px] overflow-hidden border border-gray-300 rounded"
+      class="relative p-3 border-b flex flex-row justify-between gap-1 border-contents-content-secondary"
+
     >
+      <div
+        class="w-[calc(100%-70px)] flex flex-col flex-shrink-1 justify-center items-start gap-1">
+        <div class="flex items-center gap-1 mb-7">
+          <span
+            class="text-label-sm px-1 bg-bluegray-600 text-white rounded"
+            >질의응답</span>
+          <span
+            class="text-label-sm px-1 bg-tertiary text-white rounded"
+            >${category}</span>
+        </div>
+        <a href=""
+          class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary">
+          <span class="absolute top-8 left-3 w-[70%] overflow-hidden whitespace-nowrap text-ellipsis">${title}</span>
+        </a>
+        <textarea
+          class="description w-full text-paragraph-sm font-normal text-gray-600 bg-transparent overflow-hidden whitespace-nowrap text-ellipsis resize-none"
+          >${description}</textarea>
+        <span class="text-paragraph-sm font-normal text-gray-600"
+          >연희동 · ${new Date(
+            created
+          ).toLocaleDateString()} · 조회 ${views} · 댓글 0</span>
+      </div>
+      <div
+        class="w-[70px] min-w-[70px] flex justify-center items-center">
+      ${
+        imgUrl === ''
+          ? ''
+          : /* html */ `<div
+      class="w-[60px] h-[60px] overflow-hidden border border-gray-300 rounded">
       <img
-        src="${imgUrl[0]}"
-        alt="이미지"
-      />
+      class="w-full h-full object-cover"
+        src="${imgUrl}"
+        alt="썸네일" />
+    </div>`
+      }
+      </div>
     </div>
-  </div>
-</div>
-</li>
+    </li>
 `;
-    qnaTemplateArray.push(template);
+  return template;
+}
+function createData(array) {
+  const result = [];
+  array.forEach((item) => {
+    const { type } = item;
+    if (type === 'together') {
+      result.push(createTogetherTemplate(item));
+    }
+    if (type === 'qna') {
+      result.push(createQnaTemplate(item));
+    }
   });
-  const resultTemplate = [...togetherTemplateArray, ...qnaTemplateArray].sort(
-    (a, b) => a.createdAt - b.createdAt
-  );
-  return resultTemplate.join('');
+  return result;
 }
 
-const boardList = document.querySelector('#board-list');
+function render(array) {
+  const boardList = getNode('#board-list');
+  boardList.innerHTML = '';
+  insertLast(boardList, array.join(''));
+}
+function getFilterString(interests) {
+  const nameTable = {
+    programming: '프로그래밍',
+    design: '인터렉션 디자인',
+    uiux: 'UI/UX',
+    frontend: '프론트엔드',
+    backend: '백엔드',
+    publishing: '퍼블리싱',
+    ai: 'AI',
+    blockchain: '블록체인',
+  };
+  const keyValueArray = Object.entries(interests);
+  const filteredArray = keyValueArray.filter(([_, value]) => value);
+  if (filteredArray.length === 0) return '';
+  const stringArray = [];
+  filteredArray.forEach(([key]) => {
+    stringArray.push(`category = "${nameTable[key]}"`);
+  });
+  return stringArray.join('||');
+}
 
-boardList.insertAdjacentHTML('beforeend', createTemplate(dummyData));
+async function getData() {
+  const filterString = getFilterString(interestsState);
+  console.log(filterString);
+  const togetherResponse = await pb.collection('together').getFullList();
+  const qnaResponse = await pb.collection('qAndA').getFullList({
+    filter: filterString,
+  });
+  const sortResponse = [...togetherResponse, ...qnaResponse].sort(
+    (a, b) => new Date(b.created).getTime() - new Date(a.created).getTime()
+  );
+  console.log(sortResponse);
+  render(createData(sortResponse));
+}
+getData(interestsState);
 
-const $modalDimmed = document.querySelector('#modalDimmed');
-const $openModalButton = document.querySelector('#openModal');
-const $closeModalButton = document.querySelector('#closeModal');
+// function createTemplate(data) {
+//   const { together, qna } = data;
+//   const togetherTemplateArray = [];
+//   const qnaTemplateArray = [];
+//   together.forEach((item) => {
+//     const { title, age, datetime, local, createdAt, maxMember, currentMember } =
+//       item;
+//     const template = /* html */ `
+//     <li  class="hover:bg-gray-100 transition-all">
+//     <div
+//       class="relative p-3 flex flex-col justify-center items-start gap-1 border-b border-contents-content-secondary"
+
+//     >
+//       <span
+//         class="text-label-sm px-1 mb-7 bg-bluegray-600 text-white rounded"
+//         >같이해요</span
+//       >
+//       <a href="/src/pages/board/togetherView.html"
+//         class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary "
+//       >
+//         <span class="absolute top-8 left-3 w-[90%] overflow-hidden whitespace-nowrap text-ellipsis">${title}</span>
+//       </a>
+//       <span
+//         class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-people_full-icon bg-no-repeat bg-left"
+//         >${age}</span
+//       >
+//       <span
+//         class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-calender-icon bg-no-repeat bg-left"
+//         >${datetime}</span
+//       >
+//       <div class="w-full flex justify-between">
+//         <span class="text-paragraph-sm font-normal text-gray-600"
+//           >${local} · 9분 전</span
+//         >
+//         <span
+//           class="pl-4 text-paragraph-sm font-normal text-gray-600 bg-people-icon bg-no-repeat bg-left"
+//           >${currentMember.length}/${maxMember}명</span
+//         >
+//       </div>
+//     </div>
+//     </li>
+//     `;
+//     togetherTemplateArray.push(template);
+//   });
+//   qna.forEach((item) => {
+//     const { title, description, local, createdAt, views, imgUrl } = item;
+//     const template = /* html */ `
+// <li class="hover:bg-gray-100 transition-all">
+// <div
+//   class="relative p-3 border-b flex flex-row justify-between gap-1 border-contents-content-secondary"
+
+// >
+//   <div
+//     class="w-[calc(100%-70px)] flex flex-col flex-shrink-1 justify-center items-start gap-1"
+//   >
+//     <div class="flex items-center gap-1 mb-7">
+//       <span
+//         class="text-label-sm px-1 bg-bluegray-600 text-white rounded"
+//         >질의응답</span
+//       >
+//       <span
+//         class="text-label-sm px-1 bg-tertiary text-white rounded"
+//         >인기</span
+//       >
+//     </div>
+//     <a href=""
+//       class="absolute top-0 left-0 w-full h-full flex-auto text-paragraph-md font-normal text-contents-content-primary "
+//     >
+//       <span class="absolute top-8 left-3 w-[70%] overflow-hidden whitespace-nowrap text-ellipsis">${title}</span>
+//     </a>
+//     <span
+//       class="w-full text-paragraph-sm font-normal text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis"
+//       >${description.slice(0, 50)}...</span
+//     >
+//     <span class="text-paragraph-sm font-normal text-gray-600"
+//       >${local} · 9분 전 · 조회 ${views}</span
+//     >
+//   </div>
+//   <div
+//     class="w-[70px] min-w-[70px] flex justify-center items-center"
+//   >
+//     <div
+//       class="w-[60px] h-[60px] overflow-hidden border border-gray-300 rounded"
+//     >
+//       <img
+//         src="${imgUrl[0]}"
+//         alt="이미지"
+//       />
+//     </div>
+//   </div>
+// </div>
+// </li>
+// `;
+//     qnaTemplateArray.push(template);
+//   });
+//   const resultTemplate = [...togetherTemplateArray, ...qnaTemplateArray].sort(
+//     (a, b) => a.createdAt - b.createdAt
+//   );
+//   return resultTemplate.join('');
+// }
+
+// boardList.insertAdjacentHTML('beforeend', createTemplate(dummyData));
+
+const $modalDimmed = getNode('#modalDimmed');
+const $openModalButton = getNode('#openModal');
+const $closeModalButton = getNode('#closeModal');
 
 function showModal() {
   const tl = gsap.timeline();
@@ -218,13 +304,28 @@ function closeModal(e) {
         gsap.set('#modal', { clearProps: 'all' });
       },
     });
+    getData();
   }
 }
-
 $openModalButton.addEventListener('click', showModal);
 $closeModalButton.addEventListener('click', closeModal);
 $modalDimmed.addEventListener('click', closeModal);
 
+const $checkButtons = getNodes('input[type="checkbox"]');
+function handleCheck({ target }) {
+  const { id } = target;
+  const label = target.nextElementSibling;
+  if (target.checked) {
+    label.textContent = '팔로우 취소';
+    interestsState[id] = true;
+  } else {
+    label.textContent = '팔로우';
+    interestsState[id] = false;
+  }
+}
+$checkButtons.forEach((button) => {
+  button.addEventListener('change', handleCheck);
+});
 /*
   TODO : 모달창 focus trap 기능 구현해야함
 */
