955b7578fe16e73a1b1db17041d6e56f229560b7
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-01-12 17:51:09 +0900

    feat: 가격 제안 js 구현
    가격 제안 페이지의 하트 클릭되도록 구현

--- src/pages/exchange/exchangeWrite.js
+++ src/pages/exchange/exchangeWrite.js
@@ -1,3 +1,27 @@
-import Pocketbase  from "pocketbase";
+import PocketBase from 'pocketbase';
+import { getNode, getNodes } from "/src/lib";
+import initInput from '../../components/ValidationInput/ValidationInput';
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+const prev = getNode('#prev');
+
+const tradeButton = getNodes('#tradeMethod > button')
+
+async function methodInfo() {
+  const hash = window.location.hash.slice(1);
+  const data = await pb.collection('selling').getOne(hash);
+
+  if (data.tradingType === 'sell') {
+    tradeButton[0].style.backgroundColor = '#373F67';
+    tradeButton[0].style.color = 'white';
+  } else {
+    tradeButton[1].style.backgroundColor = '#373F67';
+    tradeButton[1].style.color = 'white';
+  }
+}
+
+initInput()
+
+
+prev.addEventListener('click', () => history.back())
+methodInfo()
