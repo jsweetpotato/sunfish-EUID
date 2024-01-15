66a548296ee4ad867aeab27229362e441548dd95
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-01-15 20:54:28 +0900

    refactor: 버튼 스타일 수정 및 전체적인 스타일 js코드 수정
    에러 수정 및 버튼 위치 수정 하트 버튼 수정

--- src/pages/exchange/exchangeData.js
+++ src/pages/exchange/exchangeData.js
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
 import { getNode, getPbImageURL, comma } from '/src/lib';
+import gsap from 'gsap';
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 const section = getNode('.content');
@@ -30,17 +31,16 @@ export default async function imageList() {
   const result = await pb
     .collection('selling')
     .getFullList('pb/collections/selling');
-
-  result.forEach((value) => {
-    const sharing =
-      value.tradingType === 'nanum'
-        ? `<span class="text-label-sm text-white rounded-[4px] gap-2 grow bg-no-repeat py-[2px] px-1 bg-secondary">나눔</span>`
-        : '';
-    section.insertAdjacentHTML(
-      'afterbegin',
-      /* html */
-      `
-      <div class="flex p-3 gap-3 justify-center items-center border-b cursor-pointer relative" id="${
+      result.forEach((value) => {
+        const sharing =
+          value.tradingType === 'nanum'
+            ? `<span class="text-label-sm text-white rounded-[4px] gap-2 grow bg-no-repeat py-[2px] px-1 bg-secondary">나눔</span>`
+            : '';
+        section.insertAdjacentHTML(
+          'afterbegin',
+          /* html */
+          `
+      <div class="exchange-list flex p-3 gap-3 justify-center items-center border-b cursor-pointer relative" id="${
         value.id
       }">
         <figure>
@@ -49,10 +49,10 @@ export default async function imageList() {
             'productImages'
           )}" alt="${value}" class=" w-[95px] h-[95px] object-cover grow rounded-lg ">
         </figure>
-        <div class="flex flex-col grow p-3 ">
+        <div class="flex flex-col grow p-3">
           <a href="/src/pages/exchange/exchangeDetail.html#${value.id}" 
           class="text-paragraph-md absolute w-full h-full top-0 left-0">
-          <span class='absolute top-5 left-32'>${value.title}</span>
+          <span class='absolute top-5 left-[129.5px]'>${value.title}</span>
           </a>
           <span class="text-paragraph-sm" aria-label="판매위치 • 작성시간">마포구 신수동 • 44분전</span>
           <div class="gap-1 grow w-full items-center">
@@ -61,15 +61,21 @@ export default async function imageList() {
               value.price
             )}원</span>
           </div>
-          <button type="button" class="heartContainer flex h-8 gap-[1px] min-w-8 absolute right-3 bottom-2 pb-1 items-center justify-end">
+          <button type="button" class="heartContainer flex h-8 gap-[1px] min-w-8 absolute right-3 bottom-2 pt-6 items-center justify-end">
             <span class="heart-bg bg-heart-icon p-2 bg-contain bg-no-repeat" aria-label="좋아요 표시하기"></span>
             <span class="text greatCount text-paragraph-sm text-right font-sans heartCount">0</span>
           </button>
         </div>
       </div>
       `
-    );
-  });
-}
+        );
+      });
+    
+    gsap.from('.exchange-list', {
+      x: -500,
+      duration: 0.3,
+      stagger: 0.1,
+    });
+};
 
 section.addEventListener('click', handleHeartClick);
